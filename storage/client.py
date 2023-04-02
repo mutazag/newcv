0a2ac77f3cce03388834ef481244dccfc81a7fff
Mutaz Abu Ghazaleh <10687960+mutazag@users.noreply.github.com>
2023-04-02 22:55:01 +1000

    sasForBlob function since public access policy was removed from blob account

--- storage/client.py
+++ storage/client.py
@@ -1,5 +1,43 @@
 from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
 from datetime import datetime, timedelta
+import os
+
+from urllib.parse import urlparse, urljoin
+
+
+def sasForBlob(urlBlob):
+    # split the url into parts
+    urlparts = urlparse(urlBlob)
+    # get the account name
+    account_name = urlparts.netloc.split('.')[0]
+    # get the container name
+    container_name = urlparts.path.split('/')[1]
+    # get the blob name
+    blob_name = urlparts.path.split('/')[2]
+
+    # return the sas token
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT')
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY')
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER')
+
+    if BLOB_ACCOUNT == account_name and BLOB_CONTAINER == container_name:
+        blob_sas = generate_blob_sas(
+            account_name=account_name,
+            account_key=BLOB_STORAGE_KEY,
+            container_name=container_name,
+            blob_name=blob_name,
+            permission=BlobSasPermissions(read=True),
+            expiry=datetime.utcnow() + timedelta(hours=1))
+        #use add sas_token as query string param to urlBlob
+        urlBlob = urljoin(urlBlob, f'?{blob_sas}')
+        return urlBlob
+
+
+
+    else:
+        return urlBlob
+
+
 
 
 def listFiles(account_name, account_key, container_name, prefix=None, delimiter=None):
@@ -24,6 +62,7 @@ def listFiles(account_name, account_key, container_name, prefix=None, delimiter=
     blob_list = list(blob_list)
 
     return list1
+
 
 
 def uploadFile(account_name, account_key, container_name, file):
