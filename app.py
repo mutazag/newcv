26358d01ea98ddc3b01a82a11abfec3381223c62
Mutaz Abu Ghazaleh <10687960+mutazag@users.noreply.github.com>
2023-04-08 12:13:08 +1000

    added files cart to session, uploaded files are now added to cart, implementing the handling to clear the cart and placeholder method to process selected cart. cart.html displays items in cart as checkboxes, and can trigger clearing the cart or processing selected items

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from flask import Flask, render_template
 from flask import request, redirect
 from flask import url_for, abort, send_from_directory
 from flask_dropzone import Dropzone
+from flask import session
+# from flask_session import Session
 import os
 from settings import read_settings
-from storage.client import listFiles, uploadFile
+from storage.client import listFiles, uploadFile, get_blobparts
 from dotenv import load_dotenv
 import logging
 
@@ -17,7 +19,9 @@ load_dotenv()
 BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT')
 BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY')
 BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER')
-#log env vars
+
+SESSION_GUID = os.environ.get('SESSION_GUID', '1234')
+# log env vars
 logging.info(f'BLOB_ACCOUNT: {BLOB_ACCOUNT}')
 logging.info(f'BLOB_STORAGE_KEY: {BLOB_STORAGE_KEY}')
 logging.info(f'BLOB_CONTAINER: {BLOB_CONTAINER}')
@@ -26,9 +30,43 @@ logging.info(f'BLOB_CONTAINER: {BLOB_CONTAINER}')
 app = Flask(__name__)
 dropzone = Dropzone(app)
 
+# configure app session
+app.secret_key = SESSION_GUID
+# clear session
+# session.clear()
+# app.config['SESSION_TYPE'] = 'filesystem'
+# Session(app)
+
+
 @app.route('/')
 def index():
-    return 'Hello, Flask!'
+
+    logging.info(f'session: {session}')
+
+    user_session = session.get('userid', [])
+    logging.info(f'user_session: {user_session}')
+    if type(user_session) is not list:
+        logging.info(f'user_session is not a list')
+        user_session = []
+
+
+
+    # append a guid to userid session
+    from uuid import uuid4
+
+    user_session.append(str(uuid4()))
+    session['userid'] = user_session
+
+
+
+    user_files = session.get('files', [])
+    logging.info(f'files: {user_files}')
+    if type(user_files) is not list:
+        logging.info(f'user_files is not a list')
+        user_files = []
+
+    # return 'Hello, Flask!'
+    return str(session['userid'])
 
 
 @app.route('/settings', methods=['GET'])
@@ -41,9 +79,38 @@ def settings():
 @app.route('/files', methods=['GET'])
 def files():
 
+    user_files = session.get('files', [])
+    logging.info(f'files: {user_files}')
+    if type(user_files) is not list:
+        logging.info(f'user_files is not a list')
+        user_files = []
+
     files = listFiles(BLOB_ACCOUNT, BLOB_STORAGE_KEY, BLOB_CONTAINER, 'prefix', 'delimiter')
     return render_template('files.html', files=files)
 
+@app.route('/cart', methods=['GET'])
+def cart():
+    files = session.get('files', [])
+    logging.info(f'files: {files}')
+
+    # {{file.blob_url}}?{{file.blob_sas}}">{{file.blob_name}}
+    cart_files = [get_blobparts(f) for f in files]
+    return render_template('cart.html', files=cart_files)
+
+
+@app.route('/cart/clear', methods=['POST'])
+def clear_cart():
+    # clear files in session and refresh page to reload cart
+    session['files'] = []
+    render_template('cart.html', files=[])
+    # return '', 204
+
+
+@app.route('/cart/process', methods=['POST'])
+def process_cart():
+    selected_images = request.form.getlist('selected-images')
+    logging.info(f'selected_images: {selected_images}')
+    return '', 204
 
 @app.route('/files', methods=['POST'])
 def upload_file():
@@ -56,7 +123,18 @@ def upload_file():
     Content Type: {upload_file.content_type}\n
     Filename: {filename}''')
 
-    uploadFile(BLOB_ACCOUNT, BLOB_STORAGE_KEY, BLOB_CONTAINER, upload_file)
+    uploaded_file_url = uploadFile(BLOB_ACCOUNT, BLOB_STORAGE_KEY, BLOB_CONTAINER, upload_file)
+    logging.info(f'uploaded_file_url: {uploaded_file_url}')
+
+    if 'files' not in session:
+        session['files'] = []
+
+    user_files = session.get('files', [])
+    user_files.append(uploaded_file_url)
+    session['files'] = user_files
+    # log user_files
+    logging.info(f'user_files: {user_files}')
+
     # return redirect(url_for('files'))
     return '', 204
 
